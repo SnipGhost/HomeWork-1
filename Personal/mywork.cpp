d7316ff48bccc165309f7fa6d25dd0855db14ee0
SnipGhost <snipghost@list.ru>
2016-12-26 13:50:37 +0300

    Maked individual task for Nastya

--- Personal/mywork.cpp
+++ Personal/mywork.cpp
@@ -1,6 +1,6 @@
 ﻿//---------------------------------------------------------------------------
 // File: mywork.cpp
-// By:   SnipGhost
+// By:   Nastya
 //                                                     Индивидуальное задание
 //---------------------------------------------------------------------------
 #include "mywork.h"
@@ -9,29 +9,24 @@
 //---------------------------------------------------------------------------
 Text *doMyHomeWork(Text *text)
 {
-	cout << "Вариант 11" << endl;
+	cout << "Вариант 09" << endl;
 	cout << "Оставить  в предложениях слова, отличающиеся от последнего\n";
-	cout << "слова предложения и совпадающие с конечным отрезком алфавита.";
+	cout << "слова предложения, в которых буквы упорядочены по алфавиту.";
 	cout << "\n\nИсходный текст: \n" << text;
 	cout << "\nИзмененный текст: \n";
 	for (int i = 0; i < text->size; ++i)
 	{
-		bool jack = false;
 		Word *last = text->sent[i]->word[text->sent[i]->size-1];
 		for (int j = 0; j < text->sent[i]->size; ++j)
 		{
 			Word *w = text->sent[i]->word[j];
 			if (strcmp(w->symbols, last->symbols) == 0) w->flag = true;
-			for (int k = 0; (k < strlen(w->symbols))&&(!w->flag); ++k)
-				if (w->symbols[k] != 'z'-strlen(w->symbols)+1+k && 
-					w->symbols[k] != 'Z'-strlen(w->symbols)+1+k ) 
+			for (unsigned k = 1; (k < strlen(w->symbols))&&(!w->flag); ++k)
+				if (w->symbols[k] < w->symbols[k-1]) 
 					w->flag = true;
-			if (!w->flag) {
+			if (!w->flag)
 				cout << *(w) << " ";
-				jack = true;
-			}
 		}
-		if (jack) cout << "\b. ";
 	}
 	cout << "\n\n";
 	return text;
